194e15305de784e934897ca454fa4da9240307ea
Heru Triana <58533039+Herutriana44@users.noreply.github.com>
2023-04-12 16:47:22 +0700

    Add files via upload

---
+++ Haurgeulis/Asset/fungsi/lanjut.js
@@ -0,0 +1,40 @@
+document.getElementById('n1').onclick = function change_image()
+	{
+		document.getElementById("foto").src="Asset/Image/1.jpg";
+		document.getElementById('caption').textContent = "Stasiun Haurgeulis";
+	}
+document.getElementById('n2').onclick = function change_image()
+	{
+		document.getElementById("foto").src="Asset/Image/2.jpg";
+		document.getElementById('caption').textContent = "Sunset Diwadukan";
+	}
+document.getElementById('n3').onclick = function change_image()
+	{
+		document.getElementById("foto").src="Asset/Image/3.jpg";
+		document.getElementById('caption').textContent = "Radio Prima FM";
+	}
+document.getElementById('n4').onclick = function change_image()
+	{
+		document.getElementById("foto").src="Asset/Image/4.jpg";
+		document.getElementById('caption').textContent = "Kantor Camat Haurgeulis";
+	}
+document.getElementById('n5').onclick = function change_image()
+	{
+		document.getElementById("foto").src="Asset/Image/5.jpg";
+		document.getElementById('caption').textContent = "Masjid Al-furqon Haurgeulis";
+	}
+document.getElementById('n6').onclick = function change_image()
+	{
+		document.getElementById("foto").src="Asset/Image/6.jpg";
+		document.getElementById('caption').textContent = "Waduk Cipancuh";
+	}
+document.getElementById('n7').onclick = function change_image()
+	{
+		document.getElementById("foto").src="Asset/Image/7.jpg";
+		document.getElementById('caption').textContent = "Rumah tua";
+	}
+document.getElementById('n8').onclick = function change_image()
+	{
+		document.getElementById("foto").src="Asset/Image/8.jpg";
+		document.getElementById('caption').textContent = "Mini alun-alun";
+	}
